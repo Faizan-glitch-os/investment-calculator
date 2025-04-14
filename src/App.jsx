d68f70d4f3d25c7190f2acbce54a3c825f2c6de6
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-14 13:02:36 +0500

    all input fields are done and value can be entered now, only the table and calculation remains

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ function App() {
   return (
     <>
       <Header />
-      <div id="user-input">
-        <UserInput label="Initial Investment" />
-        <UserInput label="Annual Investment" />
-        <UserInput label="Expected Return" />
-        <UserInput label="Duration" />
-      </div>
+      <UserInput />
     </>
   );
 }
--- src/components/user-input.jsx
+++ src/components/user-input.jsx
@@ -1,8 +1,71 @@
-export default function UserInput({ label }) {
+import { useState } from "react";
+
+const initialData = {
+  initial: 1000,
+  annual: 2000,
+  expected: 3000,
+  duration: 12,
+};
+
+export default function UserInput() {
+  const [value, setValue] = useState(initialData);
+
+  function handleOnChange(whichValue, newValue) {
+    setValue((previosValue) => {
+      return { ...previosValue, [whichValue]: newValue };
+    });
+  }
+
   return (
-    <div>
-      <label htmlFor="userInput">{label}</label>
-      <input type="number" step="1" id="userInput" />
-    </div>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="initial">Initial Investment</label>
+          <input
+            type="number"
+            id="initial"
+            step="1"
+            required
+            value={value.initial}
+            onChange={(event) => handleOnChange("initial", event.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="annaul">Annual Investment</label>
+          <input
+            type="number"
+            id="annaul"
+            step="1"
+            required
+            value={value.annual}
+            onChange={(event) => handleOnChange("annual", event.target.value)}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected">Expected Returns</label>
+          <input
+            type="number"
+            id="expected"
+            step="1"
+            required
+            value={value.expected}
+            onChange={(event) => handleOnChange("expected", event.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Duration</label>
+          <input
+            type="number"
+            id="duration"
+            step="1"
+            required
+            value={value.duration}
+            onChange={(event) => handleOnChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
   );
 }
